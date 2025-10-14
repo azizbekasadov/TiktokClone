a85146cc394791cfc67173b448ef5a46fd1ffa69
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-14 23:45:36 +0200

    Initial Commit

---
+++ TiktokCloneTests/TiktokCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokCloneTests.swift
+//  TiktokCloneTests
+//
+//  Created by Azizbek Asadov on 14.10.2025.
+//
+
+import Testing
+@testable import TiktokClone
+
+struct TiktokCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
